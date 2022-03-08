e9cbfbc0f7064823c2541ecd7bb1a275266051a1
madhurishriniwar73 <madhushriniwar@gmil.com>
2022-03-08 20:01:27 +0530

    javascrpt created

---
+++ html/index.js
@@ -0,0 +1,20 @@
+//Starting with javaScript
+
+//var let const
+//variable declaration
+//VAR
+var a;
+a=10;
+console.log(a);
+a="hello";
+console.log(a);
+//typeof operator->tells type of data stored 
+console.log(typeof a);
+
+a=true;
+console.log(a);
+console.log(typeof a)
+
+a=null;
+console.log(a);
+console.log(typeof a)
