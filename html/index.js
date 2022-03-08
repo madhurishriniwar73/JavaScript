68177fa8482d8f3f252de9153019709484515725
madhushriniwar73 <madhushriniwar@gmail.com>
2022-03-08 20:25:05 +0530

    Datatypes done

--- html/index.js
+++ html/index.js
@@ -18,3 +18,24 @@ console.log(typeof a)
 a=null;
 console.log(a);
 console.log(typeof a)
+
+
+//var
+var num=10;
+console.log(num);
+var float=2.4;
+console.log(float);
+
+//Boolean
+var t=true;
+var f=false;
+console.log(t);
+console.log(f);
+
+//String
+// ' '," ",``
+//single double and backtick
+var str='a';
+console.log(typeof str);
+str="How you doin";
+console.log(str);
